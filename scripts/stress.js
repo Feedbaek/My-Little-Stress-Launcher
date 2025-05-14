d1a3d7b14752f6347c33c2c425fa296becc99e92
Feedbaek <nicek789@naver.com>
2025-05-14 15:00:57 +0900

    Feat: 테스트 시나리오 2개로 변경

--- scripts/stress.js
+++ scripts/stress.js
@@ -1,9 +1,34 @@
 import http from 'k6/http';
-import { sleep } from 'k6';
-export const options = {
-  vus: 200,
-  duration: '30s',
+import { check, sleep } from 'k6';
+
+export let options = {
+  scenarios: {
+    five_vus_api: {
+      executor: 'constant-vus',
+      exec: 'fiveGuys',
+      vus: 5,
+      duration: '1m',
+    },
+    ten_vus_api: {
+      executor: 'ramping-vus',
+      exec: 'tenGuys',
+      startVUs: 0,
+      stages: [
+        { duration: '30s', target: 10 },
+        { duration: '30s', target: 0  },
+      ],
+    },
+  },
 };
-export default function () {
-  http.get('http://host.docker.internal:8080/api/hello');
+
+export function fiveGuys() {
+  let res = http.get('http://host.docker.internal:8080/api/hello');
+  check(res, { 'fiveGuys ok': (r) => r.status === 200 });
+  sleep(1);
+}
+
+export function tenGuys() {
+  let res = http.get('http://host.docker.internal:8080/api/hello');
+  check(res, { 'tenGuys ok': (r) => r.status === 200 });
+  sleep(1);
 }
